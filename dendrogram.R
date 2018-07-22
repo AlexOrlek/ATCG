d215a609fcaae82109240794c4bf708f634c22a3
Alex Orlek <alex.orlek@wolfson.ox.ac.uk>
2018-07-22 21:30:13 +0100

    more flexible selection of distance score column input for dendrogram

--- dendrogram.R
+++ dendrogram.R
@@ -1,107 +1,104 @@
 args = commandArgs(trailingOnly=TRUE)
 library('gsubfn')
-#library('dplyr')
-#library('ggdendro')
-#library('ggplot2')
-#library('cowplot')
-#library('rwantshue')
 
-#args[1] is filepath to output folder, args[2] is distance score column name e.g. DistanceScore_d9
+#args[1] is filepath to output folder; subsequent argument(s) are distance score columns e.g. DistanceScore_d9
 
-myreport<-read.table(gsubfn('%1', list('%1'=args[1]),'%1/output/alldistancestats.tsv'), header = TRUE, sep='\t')
-writefilepath=gsubfn('%1|%2', list('%1'=args[1],'%2'=args[2]), '%1/output/dendrogram_%2.pdf')
-
+myreport<-read.table(gsubfn('%1', list('%1'=args[1]),'%1/output/distancestats.tsv'), header = TRUE, sep='\t')
 
 #order data by sample name
 myreport<-myreport[order(myreport$Sample1,myreport$Sample2),]
 
 #edit sample names
-myreport$Sample1<-sapply(as.list(as.vector(myreport$Sample1)), function(x) substr(x, 1, nchar(x)-37))
-myreport$Sample2<-sapply(as.list(as.vector(myreport$Sample2)), function(x) substr(x, 1, nchar(x)-37)) #OLD:function(x) paste(substr(x, 1, nchar(x)-39),substr(x,nchar(x),nchar(x)),sep='_')
+#myreport$Sample1<-sapply(as.list(as.vector(myreport$Sample1)), function(x) substr(x, 1, nchar(x)-37))
+#myreport$Sample2<-sapply(as.list(as.vector(myreport$Sample2)), function(x) substr(x, 1, nchar(x)-37)) #OLD:function(x) paste(substr(x, 1, nchar(x)-39),substr(x,nchar(x),nchar(x)),sep='_')
+
+distargs=args[2:length(args)]
+
+for (distarg in distargs) {
+  ###make distance matrix
+  samples1<-as.character(sort(unique(myreport$Sample1)))
+  samples<-sort(unique(c(as.character(unique(myreport$Sample1)),as.character(unique(myreport$Sample2)))))
+  myreportmatrix<-matrix(NA, ncol=length(samples), nrow=length(samples))
+  myreportlist<-list()
+
+  #first make list of samples1:samples2/scores
+  colnamesmyreport<-colnames(myreport)
+  #plasmidscorecols<-c(which(colnamesmyreport=='Sample2'),which(colnamesmyreport=='DistanceScore_d6')) #OLD - now using d9 (logarithmic scale, robust to differences in genome size)
+  #plasmidscorecols<-c(which(colnamesmyreport=='Sample2'),which(colnamesmyreport=='DistanceScore_d9'))
+  plasmidscorecols<-c(which(colnamesmyreport=='Sample2'),which(colnamesmyreport==distarg))
+  for (i in 1:length(samples1))  {
+    myreportlist[[i]]<-myreport[myreport$Sample1==samples1[i],plasmidscorecols]
+  }
+  names(myreportlist)<-samples1
 
 
-###make distance matrix
-samples1<-as.character(sort(unique(myreport$Sample1)))
-samples<-sort(unique(c(as.character(unique(myreport$Sample1)),as.character(unique(myreport$Sample2)))))
-myreportmatrix<-matrix(NA, ncol=length(samples), nrow=length(samples))
-myreportlist<-list()
+  #make matrix
 
-#first make list of samples1:samples2/scores
-colnamesmyreport<-colnames(myreport)
-#plasmidscorecols<-c(which(colnamesmyreport=='Sample2'),which(colnamesmyreport=='DistanceScore_d6')) #OLD - now using d9 (logarithmic scale, robust to differences in genome size)
-#plasmidscorecols<-c(which(colnamesmyreport=='Sample2'),which(colnamesmyreport=='DistanceScore_d9'))
-plasmidscorecols<-c(which(colnamesmyreport=='Sample2'),which(colnamesmyreport==args[2]))
-for (i in 1:length(samples1))  {
-  myreportlist[[i]]<-myreport[myreport$Sample1==samples1[i],plasmidscorecols]
-}
-names(myreportlist)<-samples1
+  #in M-k 2014 when there are no hsps, distance is set as twice maximum observed distance (rather than just setting to 1)
+  #head(rev(sort(myreport$DistanceScore_d6)),100); many distances are close to 1, so using twice max helps to distinguish samples with no similarity to others, from low similarity samples
 
+  #maxdist<-2*(max(myreport$DistanceScore_d9))
+  maxdist<-2*(max(myreport[,distarg]))
 
-#make matrix
-
-#in M-k 2014 when there are no hsps, distance is set as twice maximum observed distance (rather than just setting to 1)
-#head(rev(sort(myreport$DistanceScore_d6)),100); many distances are close to 1, so using twice max helps to distinguish samples with no similarity to others, from low similarity samples
-
-#maxdist<-2*(max(myreport$DistanceScore_d9))
-maxdist<-2*(max(myreport[,args[2]]))
-
-for(i in 1:length(samples)) {
-  sample<-samples[i]
-  if (sample %in% samples1) {
-    for(j in 1:length(samples)) {
-      if (j<=i) { #upper-right triangle or diagonal (automatically set to NA and 0 respectively)
-        next
+  for(i in 1:length(samples)) {
+    sample<-samples[i]
+    if (sample %in% samples1) {
+      for(j in 1:length(samples)) {
+	if (j<=i) { #upper-right triangle or diagonal (automatically set to NA and 0 respectively)
+	  next
+	}
+	#print(c(i,j))
+	value<-myreportlist[[sample]][which(myreportlist[[sample]][,1]==samples[j]),2]
+	#print(value)
+	if (length(value)==0) {
+	  value=maxdist #set distance to maximum (no blast hits) for given pairwise sample comparison
+	}
+	myreportmatrix[j,i]<-value #this assigns values to the lower triangle of the matrix
       }
-      #print(c(i,j))
-      value<-myreportlist[[sample]][which(myreportlist[[sample]][,1]==samples[j]),2]
-      #print(value)
-      if (length(value)==0) {
-        value=maxdist #set distance to maximum (no blast hits) for given pairwise sample comparison
+    }
+    else {
+      for(j in 1:length(samples)) {
+	if (j<=i) {
+	  next
+	}
+	#print(c(i,j))
+	value=maxdist #set distance to maximum (no blast hits)
+	myreportmatrix[j,i]<-value #this assigns values to the lower triangle of the matrix
       }
-      myreportmatrix[j,i]<-value #this assigns values to the lower triangle of the matrix
     }
   }
-  else {
-    for(j in 1:length(samples)) {
-      if (j<=i) {
-        next
-      }
-      #print(c(i,j))
-      value=maxdist #set distance to maximum (no blast hits)
-      myreportmatrix[j,i]<-value #this assigns values to the lower triangle of the matrix
-    }
+
+
+  colnames(myreportmatrix)<-samples
+  rownames(myreportmatrix)<-samples
+  myreportmatrix[upper.tri(myreportmatrix, diag=F)]<-NA #assign upper triangle values (excluding diagonals) to NA
+  diag(myreportmatrix)<-0 #assign diagnonals to 0 distance
+  d<-as.dist(myreportmatrix) #convert distance matrix to dist structure that can be used as input for hclust
+
+
+  #make ggplot dendrogram / save dendrogram to file
+  clust = hclust(d) #complete linkage used by default (and in plasmidprofiler); not using FastME which is used by M-k.  
+  clustdendro<-as.dendrogram(clust)
+  dendfilepath=gsubfn('%1|%2', list('%1'=args[1],'%2'=distarg), '%1/output/dendrogram_%2.rd')
+  saveRDS(clustdendro,dendfilepath)
+
+  #plot
+
+  numsamples<-length(samples1)
+  print(numsamples)
+  print('number of samples')
+  height=(numsamples%/%5)*5
+  width=(numsamples%/%5)*3
+  if (height<15) {
+     height=15
   }
+  if (width<10) {
+     width=10
+  }
+
+  writefilepath=gsubfn('%1|%2', list('%1'=args[1],'%2'=distarg), '%1/output/dendrogram_%2.pdf')
+  pdf(writefilepath,width,height)
+  par(mar = c(5,2,2,10)) #bottom left top right N.B this must come below pdf command 
+  plot(clustdendro,horiz=T)
+  dev.off()
 }
-
-
-colnames(myreportmatrix)<-samples
-rownames(myreportmatrix)<-samples
-myreportmatrix[upper.tri(myreportmatrix, diag=F)]<-NA #assign upper triangle values (excluding diagonals) to NA
-diag(myreportmatrix)<-0 #assign diagnonals to 0 distance
-d<-as.dist(myreportmatrix) #convert distance matrix to dist structure that can be used as input for hclust
-
-
-#make ggplot dendrogram / save dendrogram to file
-clust = hclust(d) #complete linkage used by default (and in plasmidprofiler); not using FastME which is used by M-k.  
-clustdendro<-as.dendrogram(clust)
-dendfilepath=gsubfn('%1|%2', list('%1'=args[1],'%2'=args[2]), '%1/output/dendrogram_%2.rd')
-saveRDS(clustdendro,dendfilepath)
-
-#plot
-
-numsamples<-length(samples1)
-print(numsamples)
-print('number of samples')
-height=(numsamples%/%5)*5
-width=(numsamples%/%5)*3
-if (height<15) {
-   height=15
-}
-if (width<10) {
-   width=10
-}
-
-pdf(writefilepath,width,height)
-par(mar = c(5,2,2,10)) #bottom left top right N.B this must come below pdf command 
-plot(clustdendro,horiz=T)
-dev.off()
--- runpipeline.py
+++ runpipeline.py
@@ -1,5 +1,6 @@
 import argparse, os, sys, time
-from mymod import runsubprocess
+sys.path.append('./')
+from pythonmods import runsubprocess
 
 
 def proportion(x):
@@ -10,46 +11,74 @@ def proportion(x):
             
 
 parser = argparse.ArgumentParser(description='Run pipeline scripts')
+parser.add_argument('-q','--queries', help='Sequences, formatted as a multi-FASTA file (required)', required=True)
 parser.add_argument('-o','--out', help='Output directory (required)', required=True)
-parser.add_argument('-q','--plasmidqueries', help='Plasmid sequences for all samples, formatted as a multi-FASTA file (required)', required=True)
-parser.add_argument('-d','--plasmiddbs', help='Text file containing filepaths to BLAST databases for each sample (required)', required=True)
-parser.add_argument('-a','--analysislevel', help='Is the analysis conducted at the "individual" plasmid level or "samplelevel" (required)', required=True)
+parser.add_argument('-f','--fastafiles', help='Text file containing filepaths to fasta files for each genome (default: fastafilepaths.tsv created in output directory)', required=False)
+parser.add_argument('-d','--blastdbs', help='Text file containing filepaths to BLAST databases for each genome (default: blastdbfilepaths.tsv created in output directory)', required=False)
 parser.add_argument('-e','--evalue', help='BLAST e-value cutoff (default: 1e-8)', default=1e-8, type=float) #1e-8 is used in ggdc web pipeline - see Meier-Kolthoff 2014
 parser.add_argument('-w','--wordsize', help='BLAST word size (default: 38)', default=38, type=int) #38 is used in ggdc web pipleine?
 parser.add_argument('-t','--threads', help='Number of threads to use (default: 8)', default=8, type=int)
-parser.add_argument('-s','--distscore', help='Distance score to use (default: DistanceScore_d8d9)', default='DistanceScore_d8d9', type=str)
+parser.add_argument('-s','--distscore', help='Distance score to use to construct dendrogram (can specify multiple parameters; default: DistanceScore_d8 DistanceScore_d9)', nargs='+', default=['DistanceScore_d8', 'DistaneScore_d9'], choices=['DistanceScore_d0','DistanceScore_d4','DistanceScore_d6','DistanceScore_d7','DistanceScore_d8','DistanceScore_d9'], type=str)
 args = parser.parse_args()
-
-
 outputpath=os.path.relpath(args.out, os.path.dirname(os.path.abspath(__file__)))
 
+
+
 start=time.time()
-runsubprocess(['python','runblast.py',outputpath, str(args.plasmidqueries), str(args.plasmiddbs), str(args.evalue), str(args.wordsize), str(args.threads)])
+if args.fastafiles==None and args.blastdbs==None:
+    args.fastafiles='%s/fastafilepaths.tsv'%outputpath
+    args.blastdbs='%s/blastdbfilepaths.tsv'%outputpath
+    runsubprocess(['bash','splitfasta.sh',outputpath, str(args.queries), str(args.threads)])
+    runsubprocess(['python','renamefastas.py',outputpath, str(args.queries)])
+    runsubprocess(['python','makeblastdbs.py',outputpath, str(args.fastafiles)])
+    later=time.time()
+    print(later-start, 'runtime; finished creating blast databases')
+if args.fastafiles!=None and args.blastdbs==None:
+    args.blastdbs='%s/blastdbfilepaths.tsv'%outputpath
+    runsubprocess(['python','makeblastdbs.py',outputpath, str(args.fastafiles)])   
+    later=time.time()
+    print(later-start, 'runtime; finished creating blast databases')
+runsubprocess(['python','runblast.py',outputpath, str(args.queries), str(args.blastdbs), str(args.evalue), str(args.wordsize), str(args.threads)])
 later=time.time()
 print(later-start, 'runtime; finished running blast')
-runsubprocess(['bash','splitblast.sh',outputpath,str(args.plasmiddbs)])
+runsubprocess(['bash','reformatblastoutput.sh',outputpath,str(args.blastdbs)])
 later=time.time()
 print(later-start, 'runtime; finished splitting alignments')
-runsubprocess(['bash','getseqlengths.sh',outputpath, str(args.plasmidqueries)])
+runsubprocess(['bash','getseqlengths.sh',outputpath, str(args.queries)])
 later=time.time()
 print(later-start, 'runtime; finished getting sequence lengths')
-if str(args.analysislevel)=='individual':
-    runsubprocess(['Rscript','granges.R',outputpath, str(args.threads)])
-elif str(args.analysislevel)=='samplelevel':
-    runsubprocess(['Rscript','granges_samplelevel.R',outputpath, str(args.threads)])
-else:
-    print('unrecognised samplelevel argument')
-    sys.exit()
+runsubprocess(['Rscript','granges.R',outputpath, str(args.threads)])
 later=time.time()
 print(later-start, 'runtime; finished trimming alignments')
-if str(args.distscore)=='DistanceScore_d8d9':
-    runsubprocess(['Rscript','dendrogram.R',outputpath, 'DistanceScore_d8'])
-    later=time.time()
-    print(later-start, 'runtime; finished plotting dendrogram')
-    runsubprocess(['Rscript','dendrogram.R',outputpath, 'DistanceScore_d9'])
-    later=time.time()
-    print(later-start, 'runtime; finished plotting dendrogram')
-else:
-    runsubprocess(['Rscript','dendrogram.R',outputpath, str(args.distscore)])
-    later=time.time()
-    print(later-start, 'runtime; finished plotting dendrogram')
+dendargs=['Rscript','dendrogram.R',outputpath]
+dendargs.extend(args.distscore)
+runsubprocess(dendargs)
+later=time.time()
+print(later-start, 'runtime; finished plotting dendrogram using distance score(s): %s'%arg.distscore)
+                    
+
+
+
+#OLD
+#parser.add_argument('-a','--analysislevel', help='Is the analysis conducted at the "individual" plasmid level or "samplelevel" (required)', required=True)
+
+# if str(args.analysislevel)=='individual':
+#     runsubprocess(['Rscript','granges.R',outputpath, str(args.threads)])
+# elif str(args.analysislevel)=='samplelevel':
+#else:
+#    print('unrecognised samplelevel argument')
+#    sys.exit()
+
+
+
+# if str(args.distscore)=='DistanceScore_d8d9':
+#     runsubprocess(['Rscript','dendrogram.R',outputpath, 'DistanceScore_d8'])
+#     later=time.time()
+#     print(later-start, 'runtime; finished plotting dendrogram')
+#     runsubprocess(['Rscript','dendrogram.R',outputpath, 'DistanceScore_d9'])
+#     later=time.time()
+#     print(later-start, 'runtime; finished plotting dendrogram')
+# else:
+#     runsubprocess(['Rscript','dendrogram.R',outputpath, str(args.distscore)])
+#     later=time.time()
+#     print(later-start, 'runtime; finished plotting dendrogram')
