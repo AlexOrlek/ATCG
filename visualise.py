f794004c708653355d2cf2c171ef92705e4d2dac
Alex Orlek <alex.orlek@wolfson.ox.ac.uk>
2020-05-03 21:37:42 +0100

    major update to README to bring up to date with code

--- visualise.py
+++ visualise.py
@@ -35,19 +35,24 @@ def ltyint(x):
     return x
 
 
+show_all_help=False
+if '--help_all' in sys.argv:
+    show_all_help=True
+
 parser = argparse.ArgumentParser(description="ATCG: Alignment Based Tool for Comparative Genomics; visualise alignments",add_help=False)
 #Help options
 help_group = parser.add_argument_group('Help')
-help_group.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit.')
+help_group.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show a help message explaining commonly used software options.')
+help_group.add_argument('--help_all', action='help', default=argparse.SUPPRESS, help='Show a help message explaining all software options.')
 
 #Input options                                                               
 input_group = parser.add_argument_group('Input')
 input_group.add_argument('-i','--inputdir', help='Input directory containing alignment files, and (optionally) tree file(s) (required)', required=True)
-input_group.add_argument('-s','--syntax', help='File name / file path syntax for identifying alignment files and (optionally) annotation files within the input/feature directories e.g. trimmedalignments_GENOMENAME.tsv GENOMENAME.gff (required)', nargs='+', required=True)
+input_group.add_argument('-s','--syntax', help='File name / file path syntax for identifying alignment files and (optionally) annotation files within the input/feature directories e.g. bestblastalignments_GENOMENAME.tsv GENOMENAME.gff (required)', nargs='+', required=True)
 input_group.add_argument('-l','--sequencelengths', help='File specifying sequence lengths (required)', required=True)
 input_group.add_argument('-c','--comparisons', help='File specifying which comparisons to visualise, output file name, and (optionally) annotation data/tree name (required)', required=True)
 input_group.add_argument('-f','--features', help='Input directory containing feature annotation data files (optional)', required=False)
-input_group.add_argument('-p', '--seg_plots', help='A list of seg_plot objects saved as an rds file (optional)', required=False)
+input_group.add_argument('-p', '--seg_plots', help='A list of seg_plot objects saved as an rds file (optional)' if show_all_help else argparse.SUPPRESS, required=False)
 
 #Output options                                               
 output_group = parser.add_argument_group('Output')
@@ -59,55 +64,55 @@ output_group.add_argument('-o','--out', help='Output directory (required)', requ
 vis_group1 = parser.add_argument_group('Visualisation options: miscellaneous')
 #vis_group1.add_argument('--comparisontype', help='Specifies whether comparisons are between a single reference and a set of queries, or if a chain of comparisons are to be visualised (default: chain)', default='chain', choices=['chain','singlereference'], type=str)
 vis_group1.add_argument('--main', help='Title text (default: no title text)', default='NULL', type=str)
-vis_group1.add_argument('--main_pos', help='Title text position if title text is provided (default: centre)', default='centre', choices=['centre','left','right'], type=str)
-vis_group1.add_argument('--legend_orientation', help='Orientation of the alignment colour scale legend (default: vertical)', default='vertical', choices=['vertical','horizontal'], type=str)
-vis_group1.add_argument('--legend_textcex', help='Relative size of the legend text (default: 0.5)', default='0.5', type=positivefloat)
-vis_group1.add_argument('--legend_titlecex', help='Relative size of the legend title text (default: 0.6)', default='0.6', type=positivefloat)
-vis_group1.add_argument('--positivecols', help='Colours defining a colour scale reflecting percent identity of positive orientation alignments e.g. red yellow would plot high identity alignments red and low identity alignments yellow; more than 2 colours can be provided e.g. red yellow green blue (default: #b70000 #f0cccc)', nargs='+',default=['#b70000','#f0cccc'],type=str) #colour on left is darkest and represents higher identity alignments (need to reverse vector in R for this to be the case).
-vis_group1.add_argument('--negativecols', help='As above but for negative orientation alignments (default: #0000b7 #ccccf0)', nargs='+',default=['#0000b7','#ccccf0'],type=str)
+vis_group1.add_argument('--main_pos', help='Title text position if title text is provided (default: centre)' if show_all_help else argparse.SUPPRESS, default='centre', choices=['centre','left','right'], type=str)
+vis_group1.add_argument('--legend_orientation', help='Orientation of the alignment colour scale legend (default: vertical)' if show_all_help else argparse.SUPPRESS, default='vertical', choices=['vertical','horizontal'], type=str)
+vis_group1.add_argument('--legend_textcex', help='Relative size of the legend text (default: 0.5)' if show_all_help else argparse.SUPPRESS, default='0.5', type=positivefloat)
+vis_group1.add_argument('--legend_titlecex', help='Relative size of the legend title text (default: 0.6)' if show_all_help else argparse.SUPPRESS, default='0.6', type=positivefloat)
+vis_group1.add_argument('--positivecols', help='Colours defining a colour scale reflecting percent identity of positive orientation alignments e.g. red yellow would plot high identity alignments red and low identity alignments yellow; more than 2 colours can be provided e.g. red yellow green blue (default: "#9e0d0d" "#eacaca")', nargs='+',default=['#9e0d0d','#eacaca'],type=str) #colour on left is darkest and represents higher identity alignments (need to reverse vector in R for this to be the case).
+vis_group1.add_argument('--negativecols', help='As above but for negative orientation alignments (default: "#0000b7" "#c7c7ef")', nargs='+',default=['#0000b7','#c7c7ef'],type=str)
 vis_group1.add_argument('--gffannotationtype', help='Specifies which annotation type(s) (column 3 of gff3 file) to retain (default: CDS)', nargs='+',default=['CDS'],type=str)
-vis_group1.add_argument('--tree_width', help='Width of the tree area as a proportion of total plot width (default: 0.2)', default='0.2', type=positivefloat)
-vis_group1.add_argument('--tree_branch_labels_cex', help='Relative size of the tree branch labels, if present (default: 0 i.e. not plotted)', default='0', type=positivefloat)
-vis_group1.add_argument('--tree_scale', action='store_true', help='If flag is provided, a scale will be plotted for the tree (default: no tree scale)')
-vis_group1.add_argument('--seg_plot_height', help='The height of the seg_plot regions as a proportion of the comparison region (default: 0.3)', default='0.3', type=positivefloat)
+vis_group1.add_argument('--tree_width', help='Width of the tree area as a proportion of total plot width (default: 0.2)' if show_all_help else argparse.SUPPRESS, default='0.2', type=positivefloat)
+vis_group1.add_argument('--tree_branch_labels_cex', help='Relative size of the tree branch labels, if present (default: 0 i.e. not plotted)' if show_all_help else argparse.SUPPRESS, default='0', type=positivefloat)
+vis_group1.add_argument('--tree_scale', action='store_true', help='If flag is provided, a scale will be plotted for the tree (default: no tree scale)' if show_all_help else argparse.SUPPRESS)
+vis_group1.add_argument('--seg_plot_height', help='The height of the seg_plot regions as a proportion of the comparison region (default: 0.3)' if show_all_help else argparse.SUPPRESS, default='0.3', type=positivefloat)
 #vis_group1.add_argument('--seg_plot_height_unit', help='The unit of seg_plot_height. Must be a valid unit (as defined in the grid package). If "null" then height will be calculated as a proportion of the comparison region (default: null)', default='null', type=str)  #set to null for simplicity
-vis_group1.add_argument('--seg_plot_yaxis', help='Can be NULL, FALSE or numeric. In the first two cases, no y-axis is drawn for the seg_plots. If numeric, a y axis is drawn with approximately that number of ticks (default: NULL)', default='NULL', type=segplotyaxis)
-vis_group1.add_argument('--seg_plot_yaxis_cex', help='Relative size of the seg_plot_yaxis text (default: 0.5)', default='0.5', type=positivefloat)
+vis_group1.add_argument('--seg_plot_yaxis', help='Can be NULL, FALSE or numeric. In the first two cases, no y-axis is drawn for the seg_plots. If numeric, a y axis is drawn with approximately that number of ticks (default: NULL)' if show_all_help else argparse.SUPPRESS, default='NULL', type=segplotyaxis)
+vis_group1.add_argument('--seg_plot_yaxis_cex', help='Relative size of the seg_plot_yaxis text (default: 0.5)' if show_all_help else argparse.SUPPRESS, default='0.5', type=positivefloat)
 vis_group1.add_argument('--output_height', help='Specifies the height of the plot in inches; calculated automatically unless specified', type=positivefloat)
 vis_group1.add_argument('--output_width', help='Specifies the width of the plot in inches; calculated automatically unless specified', type=positivefloat)
 
 #sequence-related
 vis_group2 = parser.add_argument_group('Visualisation options: sequence-related')
 vis_group2.add_argument('--sequencefills', help='Fill colour(s) of sequence symbols; comma-separated colours indicate colours of adjacent subsequences for a given genome; space-separated colours indicate colours for the different genomes in the comparison; colour values will be replicated so that each sequence is assigned a colour e.g. "white" will assign all sequences white fill colour; "lightgray,darkgray lightblue,darkblue" will assign sequences in the first genome grayscale fill colours and sequences in the second genome blue shade fill colours (default: white)', nargs='+',default=['white'],type=str)
-vis_group2.add_argument('--sequenceoutlines', help='Outline colour(s) of sequence symbols; see sequencefills for more detailed explanation (default: black)', nargs='+',default=['black'],type=str)
-vis_group2.add_argument('--sequencetypes', help='Symbol type(s) of sequence symbols (either "blocks" or "lines"); see sequencefills for more detailed explanation (default: blocks)', nargs='+',default=['blocks'], choices=['blocks','lines'], type=str)
+vis_group2.add_argument('--sequenceoutlines', help='Outline colour(s) of sequence symbols; takes comma-/space-separated input to map outline colours to sequences (similar to --sequencefills flag) (default: black)', nargs='+',default=['black'],type=str)
+vis_group2.add_argument('--sequencetypes', help='Symbol type(s) of sequence symbols (either "blocks" or "lines"); takes comma-/space-separated input to map symbol types to sequences (similar to --sequencefills flag) (default: blocks)', nargs='+',default=['blocks'], choices=['blocks','lines'], type=str)
 vis_group2.add_argument('--dna_seg_labels', help='The names of the sequences; must provide same number of names as there are sequences. (default: use names of dna_segs if available)', default=['NULL'],nargs='+',type=str)
-vis_group2.add_argument('--dna_seg_label_cex', help='The relative size of dna_seg_labels text (default: 0.8)', default='0.8',type=positivefloat)
-vis_group2.add_argument('--dna_seg_label_col', help='The colour of dna_seg_labels text; provide either a single value or a space-separated list of the same length as the number of genomes (default: black)', default=['black'],nargs='+',type=str)
+vis_group2.add_argument('--dna_seg_label_cex', help='The relative size of dna_seg_labels text (default: 0.8)' if show_all_help else argparse.SUPPRESS, default='0.8',type=positivefloat)
+vis_group2.add_argument('--dna_seg_label_col', help='The colour of dna_seg_labels text; provide either a single value or a space-separated list of the same length as the number of genomes (default: black)' if show_all_help else argparse.SUPPRESS, default=['black'],nargs='+',type=str)
 #vis_group2.add_argument('--dna_seg_line', help='Should the line in the middle of the query segments be drawn; what colour(s) should be used; provide either true/false (case-insensitive) or colour; provide either a single value or a space-separated list of the same length as the number of genomes e.g. false would specify no line; blue would specify all lines as blue (default: false)', default=['false'],nargs='+',type=str)  ###this should always be false - can use sequencetypes to get lines rather than blocks
-vis_group2.add_argument('--minimum_gap_size', help='The space between subsequences, expressed as the proportion of plotting width taken up by gaps (default: 0.05)', default='0.05',type=positivefloat)
-vis_group2.add_argument('--scale', help='Should a global scale be shown at the bottom of the plot; provide true/false (case-insensitive) (default: false)', default='false',choices=['true','True','TRUE','false','False','FALSE'],type=str)
-vis_group2.add_argument('--dna_seg_scale', help='Should per-genome scales be shown; provide true/false (case-insensitive) (default: true)', default='true',choices=['true','True','TRUE','false','False','FALSE'],type=str)
-vis_group2.add_argument('--dna_seg_scale_cex', help='The relative size of per-genome scales (default: 0.7)', default='0.7',type=positivefloat)
-vis_group2.add_argument('--dna_seg_scale_n_ticks', help='The approximate number of ticks on the scale of the longest genome (default: 7)', default='7',type=positiveint)
+vis_group2.add_argument('--minimum_gap_size', help='The space between subsequences, expressed as the proportion of plotting width taken up by gaps (default: 0.05)' if show_all_help else argparse.SUPPRESS, default='0.05',type=positivefloat)
+vis_group2.add_argument('--scale', help='Should a global scale be shown at the bottom of the plot; provide true/false (case-insensitive) (default: false)' if show_all_help else argparse.SUPPRESS, default='false',choices=['true','True','TRUE','false','False','FALSE'],type=str)
+vis_group2.add_argument('--dna_seg_scale', help='Should per-genome scales be shown; provide true/false (case-insensitive) (default: true)' if show_all_help else argparse.SUPPRESS, default='true',choices=['true','True','TRUE','false','False','FALSE'],type=str)
+vis_group2.add_argument('--dna_seg_scale_cex', help='The relative size of per-genome scales (default: 0.7)' if show_all_help else argparse.SUPPRESS, default='0.7',type=positivefloat)
+vis_group2.add_argument('--dna_seg_scale_n_ticks', help='The approximate number of ticks on the scale of the longest genome (default: 7)' if show_all_help else argparse.SUPPRESS, default='7',type=positiveint)
 
 #annotation-related
 vis_group3 = parser.add_argument_group('Visualisation options: annotation-related')
 vis_group3.add_argument('--annotation_gene_type', help='Specifies the default symbol to represent annotations (default: side_bars)', default='side_bars', choices=['none','arrowheads','arrows','headless_arrows','blocks','bars','points','text','lines','side_blocks','side_bars','side_points','side_text','side_lines','introns','exons','side_exons'],type=str)
 vis_group3.add_argument('--annotation_outline_col', help='Specifies the default outline colour of the annotation symbol (default: gray)', default='gray',type=str)
 vis_group3.add_argument('--annotation_fill_col', help='Specifies the default fill colour of the annotation symbol (default: black)', default='black',type=str)
-vis_group3.add_argument('--annotation_lty', help='Specifies the default outline line type of the annotation symbol (default: 1)', default='1',type=ltyint)
-vis_group3.add_argument('--annotation_lwd', help='Specifies the default outline line width of the annotation symbol (default: 0.5)', default='0.5',type=positivefloat)
+vis_group3.add_argument('--annotation_lty', help='Specifies the default outline line type of the annotation symbol (default: 1)' if show_all_help else argparse.SUPPRESS, default='1',type=ltyint)
+vis_group3.add_argument('--annotation_lwd', help='Specifies the default outline line width of the annotation symbol (default: 0.5)' if show_all_help else argparse.SUPPRESS, default='0.5',type=positivefloat)
 vis_group3.add_argument('--annotationtxt_name', help='The tag name in the gff file to use for annotation text e.g. gene, product; unless specified, the gene tag will be used where available, and otherwise the product tag)', type=str)
-vis_group3.add_argument('--annotationtxt_type', help='Specifies whether annotation text positioned in the middle of the annotation range or spanning the annotation range (default: mid)', default='mid', choices=['mid','spanning'], type=str)
-vis_group3.add_argument('--annotationtxt_height', help='Specifies how much space to allocate to annotation text; calculated automatically based on annotation text length unless specified', type=positivefloat)
+vis_group3.add_argument('--annotationtxt_type', help='Specifies whether annotation text positioned in the middle of the annotation range or spanning the annotation range (default: mid)' if show_all_help else argparse.SUPPRESS, default='mid', choices=['mid','spanning'], type=str)
+vis_group3.add_argument('--annotationtxt_height', help='Specifies how much space to allocate to annotation text; calculated automatically based on annotation text length unless specified' if show_all_help else argparse.SUPPRESS, type=positivefloat)
 vis_group3.add_argument('--annotationtxt_rot', help='Specifies the annotation text rotation (default: 40)', default='40',type=int)
-vis_group3.add_argument('--annotationtxt_cex', help='Specifies the relative size of annotation text (default: 0.5)', default='0.5',type=positivefloat)
+vis_group3.add_argument('--annotationtxt_cex', help='Specifies the relative size of annotation text (default: 0.7)' if show_all_help else argparse.SUPPRESS, default='0.7',type=positivefloat)
 vis_group3.add_argument('--annotationtxt_exclusion', help='Exclusion criteria for text annotations, command line argument(s) (optional)', nargs='+',type=str)
 vis_group3.add_argument('--annotationtxt_inclusion', help='Inclusion criteria for text annotations, command line argument(s) (optional)', nargs='+',type=str)
-vis_group3.add_argument('--annotationtxt_exclusion_file', help='Exclusion criteria for text annotations, text file (optional)')
-vis_group3.add_argument('--annotationtxt_inclusion_file', help='Inclusion criteria for text annotations, text file (optional)')
-vis_group3.add_argument('--annotationtxt_exclinc_casesensitive', action='store_true', help='If flag is provided, exclusion/inclusion criteria will be case-sensitive when matching annotations (default: case-insensitive)')
+vis_group3.add_argument('--annotationtxt_exclusion_file', help='Exclusion criteria for text annotations, text file (optional)' if show_all_help else argparse.SUPPRESS)
+vis_group3.add_argument('--annotationtxt_inclusion_file', help='Inclusion criteria for text annotations, text file (optional)' if show_all_help else argparse.SUPPRESS)
+vis_group3.add_argument('--annotationtxt_exclinc_casesensitive', action='store_true', help='If flag is provided, exclusion/inclusion criteria will be case-sensitive when matching annotations (default: case-insensitive)' if show_all_help else argparse.SUPPRESS)
 
 
 
